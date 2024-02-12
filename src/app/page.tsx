2aee2264c53967ce9260bc34b9522a7097f729bb
Mahavishnu Jneesh <mahavishnu.afonso@sga.pucminas.br>
2024-02-12 16:45:26 -0300

    conclusion?

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,113 +1,235 @@
 import Image from "next/image";
+import React from "react";
+import App from "./components/App";
+
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    <main className="flex bg-gray-100">
+      <header className="w-29 h-screen bg-primary text-white min-lg:hidden">
+
+        <div className="flex justify-center items-baseline mx-auto pt-11">
+          <Image src="/logo.svg" width={47.27} height={40} alt="N Banking" />
+          <h2 className="px-0.5">Banking</h2>
+
         </div>
-      </div>
+        <nav className="">
+          <div className="ml-4 mt-11 space-y-8 ">
+            <li className="flex items-center gap-3 bg-blue-600 rounded-l-lg px-3 h-12 ">
+              <Image src="/dashboard.svg" width={30} height={30} alt="" />
+              <a href="#">Dashboard</a>
+            </li>
+            <li className="flex items-center gap-3">
+              <Image src="/perfil.svg" width={30} height={30} alt="" />
+              <a href="#">My Accounts</a>
+            </li>
+            <li className="flex items-center gap-3">
+              <Image src="/cards.svg" width={30} height={30} alt="" />
+              <a href="#">My Cards</a>
+            </li>
+            <li className="flex items-center gap-3">
+              <Image src="/transfer.svg" width={30} height={30} alt="" />
+              <a href="#">Fund Transfer</a>
+            </li>
+            <li className="flex items-center gap-3">
+              <Image src="/paper.svg" width={30} height={30} alt="" />
+              <a href="#">Bill Payment</a>
+            </li>
+            <li className="flex items-center gap-3">
+              <Image src="/settings.svg" width={30} height={30} alt="" />
+              <a href="#">Settings</a>
+            </li>
+          </div>
+        </nav>
+      </header>
 
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-full sm:before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full sm:after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
+      <section className="w-full h-scren">
 
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
+        <section className="h-19 w-full flex justify-between px-8 border-b-2 bg-white border-gray-200">
+          <div className="flex items-center">
+            <div className="flex items-end gap-3">
 
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
+              <h2 className="text-2xl text-gray-900 font-bold">Good Morning</h2>
+              <Image src="/sun.svg" width={35} height={22} alt="Sun Icon" />
 
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore starter templates for Next.js.
-          </p>
-        </a>
+            </div>
+          </div>
 
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50 text-balance`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
+          <div className="flex items-center gap-10">
+
+            <div className="w-12 h-12 flex items-center justify-center bg-blurblue rounded-full">
+              <Image className="" src="/bell.svg" width={24} height={24} alt="bell icon" />
+              <span className="font-bold">3</span>
+            </div>
+            <div className="flex items-center justify-center gap-3">
+              <Image className="" src="/avatar.png" width={48} height={48} alt="User photo" />
+
+              <h3 className="font-bold text-xl">Kathryn</h3>
+
+              <Image src="/down-arrow.svg" width={24} height={24} alt="down arrow menu" />
+            </div>
+          </div>
+        </section>
+
+        <div className="flex w-full ">
+          <div className="m-8 space-y-8">
+
+
+            <div className="w-100 h-52 bg-white rounded-md">
+
+
+              <div className="flex items-center justify-between p-6">
+                <h3 className="font-bold text-xl">Balance</h3>
+                <Image src="/more.svg" width={4} height={16} alt="more information" />
+              </div>
+
+              <div className="backgroundDiv"></div>
+
+              <div className="mt-4 pl-6 space-y-6">
+                <h2 className="font-bold text-4xl">$14,567.00 USD</h2>
+                <p className="font-semibold text-base">Available</p>
+              </div>
+
+            </div>
+            <div className=" h-64 bg-white rounded-md">
+
+              <div className="flex justify-between px-4 py-4">
+                <h2 className="font-bold text-xl">Favourite Transfers</h2>
+                <span className="font-normal text-sm py-">See All</span>
+              </div>
+
+              <div className="space-y-7 px-6">
+                <div className=" flex items-center py-3 pl-5 gap-4 border-2 border-gray/200 rounded-md">
+                  <Image className="" src="/avatar2.png" width={48} height={48} alt="User photo" />
+                  <span className="font-bold text-base">Kathryn Murphy</span>
+                </div>
+                <div className="flex items-center py-3 pl-5 gap-4 border-2 border-gray/200 rounded-md">
+                  <Image className="" src="/avatar3.png" width={48} height={48} alt="User photo" />
+                  <span className="font-bold text-base">Wade Warren</span>
+                </div>
+
+              </div>
+            </div>
+          </div>
+
+          <div className="w-full h-105 mt-8 mr-8 p-6  bg-white rounded-md ">
+            <div className="flex items-center justify-between p-4">
+              <h3 className="font-bold text-base">Insights</h3>
+              <Image src="/more.svg" width={4} height={16} alt="more information" />
+            </div>
+            <div className="flex justify-center">
+              <App />
+            </div>
+          </div>
+        </div>
+
+        <div className="min-h-85 bg-white rounded-xl mx-8" >
+            
+            <div className="flex  justify-between px-6 py-4">
+              <h3 className="font-bold text-xl">Transaction</h3>
+              <Image src="/more.svg" width={4} height={16} alt="more information" />
+            </div>
+
+            <div className="">
+              <table className="min-w-full ">
+                <thead className="bg-white">
+                  <tr className="border-2 m-8 flex items-center justify-between rounded-xl">
+                    <td scope="col" className="pl-12 py-3 text-left text-base font-semibold text-gray-800 ">
+                      Name
+                    </td>
+                    <td scope="col" className="pl-28 py-3 text-left text-base font-semibold text-gray-800 ">
+                      ID
+                    </td>
+                    <td scope="col" className="pl-12 py-3 text-left text-base font-semibold text-gray-800 ">
+                      Status
+                    </td>
+                    <td scope="col" className="px-12 py-3 text-left text-base font-semibold text-gray-800 ">
+                      Amount
+                    </td>
+                    <td scope="col" className="pr-12 py-3 text-left text-base font-semibold text-gray-800 ">
+                      Date
+                    </td>
+                  </tr>
+                </thead>
+                <tbody className="bg-white rounded-xl">
+                  <tr className="flex justify-between items-center border-2 mx-8 mb-4 rounded-xl">
+                    <td className="px-6 py-4 whitespace-nowrap">
+                      <div className="flex items-center">
+                        <div className="flex-shrink-0">
+                          <Image className="" src="/avatar4.png" width={48} height={48} alt="User photo" />
+                        </div>
+                        <div className="ml-4">
+                          <p className="font-semibold text-base text-gray-800">Jenny Wilson</p>
+                        </div>
+                      </div>
+                    </td>
+                    <td className="px-6 py-4 font-semibold text-base text-gray-800 whitespace-nowrap">
+                      <span className="">245689</span>
+                    </td>
+                    <td className="px-6 py-4 whitespace-nowrap">
+                      <span className="px-2 py-1 inline-flex text-base leading-5 font-semibold rounded-full bg-green-100 text-green-800">Complete</span>
+                    </td>
+                    <td className="px-6 py-4 whitespace-nowrap ">
+                      <span className="text-base font-semibold text-gray-800">+$455,00</span>
+                    </td>
+                    <td className="px-6 py-4 whitespace-nowrap ">
+                      <span className="text-base font-semibold text-gray-800">20 Dec 2022</span>
+                    </td>
+                  </tr>
+                  <tr className="flex justify-between items-center border-2 mx-8 mb-4 rounded-xl">
+                    <td className="px-6 py-4 whitespace-nowrap">
+                      <div className="flex items-center">
+                        <div className="flex-shrink-0">
+                          <Image className="" src="/avatar5.png" width={48} height={48} alt="User photo" />
+                        </div>
+                        <div className="ml-4">
+                          <p className="font-semibold text-base text-gray-800">Robert Fox</p>
+                        </div>
+                      </div>
+                    </td>
+                    <td className="px-6 py-4 font-semibold text-base text-gray-800 whitespace-nowrap">
+                      <span className="">245689</span>
+                    </td>
+                    <td className="px-6 py-4 whitespace-nowrap">
+                      <span className="px-2 py-1 inline-flex text-base leading-5 font-semibold rounded-full bg-green-100 text-green-800">Complete</span>
+                    </td>
+                    <td className="px-6 py-4 whitespace-nowrap ">
+                      <span className="text-base font-semibold text-gray-800">+$455,00</span>
+                    </td>
+                    <td className="px-6 py-4 whitespace-nowrap ">
+                      <span className="text-base font-semibold text-gray-800">20 Dec 2022</span>
+                    </td>
+                  </tr>
+
+                  <tr className="flex justify-between items-center border-2 mx-8 mb-4 rounded-xl">
+                    <td className="px-6 py-4 whitespace-nowrap">
+                      <div className="flex items-center">
+                        <div className="flex-shrink-0">
+                          <Image className="" src="/avatar6.png" width={48} height={48} alt="User photo" />
+                        </div>
+                        <div className="ml-4">
+                          <p className="font-semibold text-base text-gray-800">Jacob Jones</p>
+                        </div>
+                      </div>
+                    </td>
+                    <td className="px-6 py-4 font-semibold text-base text-gray-800 whitespace-nowrap">
+                      <span className="">245689</span>
+                    </td>
+                    <td className="px-6 py-4 whitespace-nowrap">
+                      <span className="px-2 py-1 inline-flex text-base leading-5 font-semibold rounded-full bg-green-100 text-green-800">Complete</span>
+                    </td>
+                    <td className="px-6 py-4 whitespace-nowrap ">
+                      <span className="text-base font-semibold text-gray-800">+$455,00</span>
+                    </td>
+                    <td className="px-6 py-4 whitespace-nowrap ">
+                      <span className="text-base font-semibold text-gray-800">20 Dec 2022</span>
+                    </td>
+                  </tr>
+                </tbody>
+              </table>
+            </div>
+        </div>
+      </section>
     </main>
   );
 }
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -11,7 +11,22 @@ const config: Config = {
       backgroundImage: {
         "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
         "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))", 
+          //'hero': "url('/public/montain2.png')"  
+      },
+      spacing: {
+        '29': '18.75rem',// 300px
+        '19': '6.25rem',// 100px
+        '85': '22.563rem',//  361px
+        '100': '26.68rem',// 427px
+        '105': '31rem',// 496px
+        '120': '38.563rem' // 617px
+
+      },
+      colors: {
+        'primary': '#0f1222',
+        'blurblue': '#F9FBFF',
+        'gray/200': '#EAECF0'
       },
     },
   },
